3d2a94bb74d9aad4976f89e37535fc269252789f
Kacian417 <knicho4492@gmail.com>
2024-10-12 00:33:51 +0000

    updated components

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 
+import Navbar from "./Navbar";
+import Jumbotron from "./Jumbotron";
+import CardRow from "./CardRow";
+import Footer from "./Footer";
 
 //create your first component
 const Home = () => {
 	return (
 		<>
-		
+			<Navbar />
+			<div className="row">
+				<div className="col-2"></div>
+				<div className="col-8">
+					<Jumbotron />
+				</div>	
+				<div className="col-2"></div>
+			</div>
+			<CardRow />
+			<Footer />
 		</>
 	);
 };
