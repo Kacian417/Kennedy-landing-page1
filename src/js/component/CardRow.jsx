6870c03b0e241a171f195e6fb64873bb1cf8e046
Kacian417 <knicho4492@gmail.com>
2024-10-12 00:57:45 +0000

    adjusted cards, added footer content

--- src/js/component/CardRow.jsx
+++ src/js/component/CardRow.jsx
@@ -5,7 +5,7 @@ import Card from './Card';
 const CardRow = () => {
     return (
         <>
-            <div className="d-flex">
+            <div className="d-flex row row-cols-1 row-cols-md-4 g-4">
                 <Card />
                 <Card />
                 <Card />
--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const Footer = () => {
     return (
         <>
-            <h1>Footer</h1>
+
+                <footer class="footer mt-5 pt-5 bg-dark">
+                    <div class="container align-center">
+                        <span class="text-light">Place sticky footer content here.</span>
+                    </div>
+                </footer>
         </>
     );
 }
